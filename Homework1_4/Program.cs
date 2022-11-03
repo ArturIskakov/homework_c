e642fe5b71a9c112e33a3070484e73be36e32e8b
Artur <iskakovan86@gmail.com>
2022-11-03 21:12:34 +0300

    Homework1_t4

---
+++ Homework1_4/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+int a = new Random().Next(-100, 100); // взял интервал от -100 до 100
+int b = new Random().Next(-100, 100);
+int c = new Random().Next(-100, 100);
+int max = a;
+Console.WriteLine(a);
+Console.WriteLine(b);
+Console.WriteLine(c);
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+Console.Write("max = ");
+Console.WriteLine(max);
