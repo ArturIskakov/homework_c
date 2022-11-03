487e5cef2ad07fe66ae1106b53113385c2e44f48
Artur <iskakovan86@gmail.com>
2022-11-03 17:37:10 +0300

    Seminar 1

--- Ex01/Program.cs
+++ Ex01/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// int sum = 0;
+// int startNumber = 1;
+
+// while (startNumber <= 10)
+// {
+//    if ( startNumber % 2 == 0 )
+//    {
+//        sum = sum + startNumber;
+//    }
+//    startNumber = startNumber + 1;
+//}
+// Console.WriteLine(sum);
+
+
+// Version 2
+int sum = 0;
+int startNumber = 2;
+
+while (startNumber <= 10)
+{
+  sum = sum + startNumber;
+  startNumber = startNumber + 2;
+}
+Console.WriteLine(sum);
