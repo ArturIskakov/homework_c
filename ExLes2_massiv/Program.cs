5c5601a8d45055dfabaf25869e8efe8104489bae
Artur <iskakovan86@gmail.com>
2022-11-04 20:46:28 +0300

    ExLes2

---
+++ ExLes2_massiv/Program.cs
@@ -0,0 +1,66 @@
+﻿// Максимум из 9 чисел
+
+/*int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+int[] array = { 14, 22, 37, 412, 59, 63, 71, 87, 59 };
+//array[0] = 12;
+
+//Console.WriteLine(array[5]);
+
+int result1 = Max(array[0], array[1], array[2]);
+int result2 = Max(array[3], array[4], array[5]);
+int result3 = Max(array[6], array[7], array[8]);
+int result = Max(result1, result2, result3);
+Console.WriteLine(result);*/
+
+// Одномерный массив array из n элементов, требуется найти элемент массива find
+
+/*int[] array = { 14, 22, 37, 412, 59, 63, 71, 87, 59 };
+
+int n = array.Length;
+
+int find = 59;
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+    }
+    index++;            //index=index+1;
+}*/
+
+// Массив с использованием Метода
+
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;              // index = index + 1;
+    }
+}
+
+void PrintArray(int[] coll)
+{
+    int count = coll.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(coll[position]);
+        position++;
+    }
+}
+
+int[] array = new int[10];   // создать новый массив в котором 10 элементов
+
+FillArray(array);
+PrintArray(array);
---
+++ ExLes2_max9/Program.cs
@@ -0,0 +1,38 @@
+﻿// Максимум из 9 чисел
+
+int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+
+
+int a1 = 43;
+int b1 = 112;
+int c1 = 70;
+int a2 = 10;
+int b2 = 19;
+int c2 = 98;
+int a3 = 93;
+int b3 = 22;
+int c3 = 820;
+
+int max1 = Max(a1, b1, c1);
+int max2 = Max(a2, b2, c2);
+int max3 = Max(a3, b3, c3);
+int max = Max(max1, max2, max3);
+
+/*if (b1 > max) max = b1;
+if (c1 > max) max = c1;
+
+if (a2 > max) max = b2;
+if (b2 > max) max = b2;
+if (c2 > max) max = c2;
+
+if (a3 > max) max = b3;
+if (b3 > max) max = b3;
+if (c3 > max) max = c3;*/
+
+Console.WriteLine(max);
