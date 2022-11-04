9d3dbabfca662c080b962642e6b3ffba51c8097e
Artur <iskakovan86@gmail.com>
2022-11-05 00:25:47 +0300

    Homework_lesson2

---
+++ Homework2_10/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// НЕЛЬЗЯ ИСПОЛЬЗОВАТЬ СТРОКИ.
+
+/*Console.Write("Введите трехзначное число и нажмите Enter: ");
+int x= Convert.ToInt32(Console.ReadLine());
+
+if (x>99 || x<999)
+{
+    int a = (x/100);
+    int b= (x-a*100)/10;
+    Console.WriteLine(b);
+}*/
+
+
+Console.Write("Введите трехзначное число и нажмите Enter: ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+if (x > 99 || x < 999)
+{
+    x=x/10;
+    Console.WriteLine(x % 10);
+}
