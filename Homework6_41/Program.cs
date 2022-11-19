77d6f0dc3bcd4e3b56450747b6394fd2f0ec6ae6
Artur <iskakovan86@gmail.com>
2022-11-20 01:18:43 +0300

    Homework Seminar 6

---
+++ Homework6_41/Program.cs
@@ -0,0 +1,16 @@
+﻿/*Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3 */
+
+Console.WriteLine("Напишите через пробел числа и нажмите Enter:");
+
+int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int count = 0;
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] > 0)
+    {
+        count++;
+    }
+}
+Console.WriteLine("Чисел больше 0 Вы ввели: " +count);
