518caef004c9dd9a7197fccfc408887a3eceb01c
Artur <iskakovan86@gmail.com>
2022-11-21 19:26:42 +0300

    Homework Seminar 7

---
+++ Homework7_50/Program.cs
@@ -0,0 +1,43 @@
+﻿/* Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+17 -> такого числа в массиве нет*/
+
+void FillandPrint(int[,] matrix)
+{
+    for (int i = 1; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 1; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(1, 20);
+            Console.Write(matrix[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.WriteLine("Введите координаты позиции элемента и нажмите Enter ");
+int m = Convert.ToInt32(Console.ReadLine());
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+int[,] matr = new int[10, 10];
+FillandPrint(matr);
+
+if (m <= matr.GetLength(0) - 1)
+{
+    if (n <= matr.GetLength(1) - 1)
+    {
+        Console.Write("Значение элемента = ");
+        Console.WriteLine(matr[m, n]);
+    }
+    /*else
+    {
+        Console.WriteLine("такого элемента в массиве нет.");
+    }*/
+}
+else
+{
+    Console.WriteLine("такого элемента в массиве нет.");
+}
---
+++ Homework7_52/Program.cs
@@ -0,0 +1,40 @@
+﻿/* Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.*/
+
+Console.WriteLine("Введите размер матрицы (количество строк и столбцов) через Enter ");
+int m = Convert.ToInt32(Console.ReadLine());
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = new int[m, n];
+Random rnd = new Random();
+void FillandPrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            matrix[i, j] = rnd.Next(0, 20);
+            Console.Write(matrix[i, j] + "\t");
+
+        }
+        Console.WriteLine();
+    }
+}
+Console.WriteLine();
+FillandPrintMatrix(matrix);
+Console.WriteLine("Среднее арифметическое элементов в каждом столбце:");
+
+for (int j = 0; j < matrix.GetLength(1); j++)
+{
+    double summ = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+
+        summ += matrix[i, j];
+    }
+    summ = summ / m;
+    Console.Write($"{summ:f1}" + "\t");
+}
