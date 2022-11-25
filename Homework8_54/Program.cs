fd9eee24e46673e3f03d4c9fa3f38891887ddc3d
Artur <iskakovan86@gmail.com>
2022-11-26 00:25:44 +0300

    Homework Seminar 8

---
+++ Homework8_54/Program.cs
@@ -0,0 +1,64 @@
+﻿/*Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+В итоге получается вот такой массив:
+7 4 2 1
+9 5 3 2
+8 4 4 2*/
+
+void FillMatrix(int[,] matr)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = matr[i, j] = rnd.Next(1, 20);
+        }
+    }
+}
+void PrintMatrix(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write(matr[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+int[,] Selectionsort(int[,] matr)
+{
+
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            for (int k = j + 1; k < matr.GetLength(1); k++)
+            {
+                if (matr[i, j] < matr[i, k])
+                {
+                    int temporary = 0;
+                    temporary = matr[i, j];
+                    matr[i, j] = matr[i, k];
+                    matr[i, k] = temporary;
+                }
+            }
+        }
+
+
+    }
+    return matr;
+
+}
+int[,] matrix = new int[3, 4];
+FillMatrix(matrix);
+PrintMatrix(matrix);
+matrix = Selectionsort(matrix);
+Console.WriteLine();
+PrintMatrix(matrix);
---
+++ Homework8_56/Program.cs
@@ -0,0 +1,51 @@
+﻿/*Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+5 2 6 7
+Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка*/
+
+void FillMatrix(int[,] matr)
+{
+    Random rnd = new Random();
+    for (int m = 0; m < matr.GetLength(0); m++)
+    {
+        for (int n = 0; n < matr.GetLength(1); n++)
+        {
+            matr[m, n] = matr[m, n] = rnd.Next(1, 20);
+        }
+    }
+}
+void PrintMatrix(int[,] matr)
+{
+    for (int m = 0; m < matr.GetLength(0); m++)
+    {
+        for (int n = 0; n < matr.GetLength(1); n++)
+        {
+            Console.Write(matr[m, n] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+int[,] matrix = new int[3, 4];
+FillMatrix(matrix);
+PrintMatrix(matrix);
+int min = int.MaxValue;
+int index = 0;
+for (int m = 0; m < matrix.GetLength(0); m++)
+{
+    int summ = 0;
+    for (int n = 0; n < matrix.GetLength(1); n++)
+    {
+
+        summ = summ + matrix[m, n];
+    }
+    if (summ < min)
+    {
+        min = summ;
+        index++;
+    }
+}
+Console.Write("Наименьшая сумма элементов в строке № ");
+Console.WriteLine(index);
